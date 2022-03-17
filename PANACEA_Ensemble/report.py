a9727978f13bc1fba13e5ec2275dfe29248e130c
malikalessa <38468857+malikalessa@users.noreply.github.com>
2022-03-17 19:17:31 +0100

    Delete PANACEA_Ensemble directory

--- PANACEA_Ensemble/report.py
+++
@@ -1,8 +0,0 @@
-def report(report_name, accuracy, confusion_matrix, classification_report ,name):
-
-
-    with open(report_name, 'a') as f:
-        print('\n\n Name : ', name, file=f)
-        print('\n Accuracy is : ', accuracy, file=f)
-        print('\n\n Confusion Matrix is : \n\n', confusion_matrix, file=f)
-        print('\n\n Classification Report is  :  \n\n', classification_report, file=f)
